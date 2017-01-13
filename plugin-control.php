93979906c61bae859ac048e5aeea25cc5b530c58
Kailey Lampert <trepmal@gmail.com>
2017-01-12 20:35:42 -0800

    general formatting

--- plugin-control.php
+++ plugin-control.php
@@ -22,38 +22,52 @@ class Plugin_Control {
 
 	function __construct() {
 
-		$this->blocklist = get_option( 'block-plugins', array() );
+		$this->blocklist   = get_option( 'block-plugins', array() );
 		$this->showblocked = get_option( 'show-blocked-plugins', false );
 
-		add_action( 'admin_init', array( $this, 'admin_init' ) );
+		add_action( 'admin_init',          array( $this, 'admin_init' ) );
 
 		// filter plugins list
-		if ( ! $this->showblocked )
-			add_filter( 'all_plugins', array( $this, 'all_plugins' ) );
+		if ( ! $this->showblocked ) {
+			add_filter( 'all_plugins',     array( $this, 'all_plugins' ) );
+		}
 		add_filter( 'plugin_action_links', array( $this, 'plugin_action_links' ), 10, 4 );
-		add_action( 'after_plugin_row', array( $this, 'after_plugin_row' ) );
+		add_action( 'after_plugin_row',    array( $this, 'after_plugin_row' ) );
 
 		// prevent back-door activation
-		add_filter( 'load-plugins.php', array( $this, 'intercept_post' ), 10, 2 );
+		add_filter( 'load-plugins.php',                 array( $this, 'intercept_post' ), 10, 2 );
 		add_filter( 'pre_update_option_active_plugins', array( $this, 'pre_update_option_active_plugins' ), 10, 2 );
 
-		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+		add_action( 'admin_menu',                       array( $this, 'admin_menu' ) );
 
 	}
 
 	function admin_init() {
-		register_setting( 'pc-opt-group', 'block-plugins', array( $this, 'sanitize' ) );
+		register_setting( 'pc-opt-group', 'block-plugins',        array( $this, 'sanitize' ) );
 		register_setting( 'pc-opt-group', 'show-blocked-plugins', array( $this, 'sanitize' ) );
 
 		add_settings_section( 'pc-section', __('', 'plugin-control' ), '__return_false', $this->pagename );
 
-		add_settings_field( 'pc-field-block-plugins', __( 'Block Plugins', 'plugin-control' ), array( $this, 'field1' ), $this->pagename, 'pc-section', $this->blocklist );
+		add_settings_field(
+			'pc-field-block-plugins',
+			__( 'Block Plugins', 'plugin-control' ),
+			array( $this, 'field1' ),
+			$this->pagename,
+			'pc-section',
+			$this->blocklist
+		);
 
-		add_settings_field( 'pc-field-show-blocked-plugins', __( 'Show Blocked Plugins', 'plugin-control' ), array( $this, 'field2' ), $this->pagename, 'pc-section', $this->showblocked );
+		add_settings_field(
+			'pc-field-show-blocked-plugins',
+			__( 'Show Blocked Plugins', 'plugin-control' ),
+			array( $this, 'field2' ),
+			$this->pagename,
+			'pc-section',
+			$this->showblocked
+		);
 	}
 
 	function sanitize( $input ) {
-		// printer( $input );
 		return $input;
 	}
 
@@ -61,7 +75,7 @@ class Plugin_Control {
 
 		$plugins = wp_list_pluck( get_plugins(), 'Name' );
 		echo '<ul>';
-		foreach( $plugins as $path => $plug ) {
+		foreach ( $plugins as $path => $plug ) {
 			$c = in_array( $path, $args ) ? ' checked="checked"' : '';
 			echo "<li><label><input type='checkbox' name='block-plugins[]' value='$path'$c /> $plug</label></li>";
 		}
@@ -76,8 +90,11 @@ class Plugin_Control {
 	}
 
 	function all_plugins( $get_plugins ) {
-		foreach( $this->blocklist as $b )
-			if ( isset( $get_plugins[ $b ] ) ) unset( $get_plugins[ $b ] );
+		foreach ( $this->blocklist as $b ) {
+			if ( isset( $get_plugins[ $b ] ) ) {
+				unset( $get_plugins[ $b ] );
+			}
+		}
 
 		return $get_plugins;
 	}
@@ -90,7 +107,7 @@ class Plugin_Control {
 		$flip = array_flip( $this->blocklist );
 		global $status;
 		if ( $status == $context && ! $this->status_change ) {
-			$this->status = $status; //preserve
+			$this->status        = $status; //preserve
 			$this->status_change = true; //flag
 		}
 		if ( isset( $flip[ $plugin_file ] ) ) {
@@ -102,6 +119,7 @@ class Plugin_Control {
 		}
 		return $actions;
 	}
+
 	// use this later hook to restore real $status
 	function after_plugin_row() {
 		if ( $this->status_change ) {
@@ -111,13 +129,15 @@ class Plugin_Control {
 	}
 
 	function intercept_post() {
-		if ( ! isset( $_REQUEST['checked'] ) ) return;
+		if ( ! isset( $_REQUEST['checked'] ) ) {
+			return;
+		}
 		$_REQUEST['checked'] = array_filter( $_REQUEST['checked'], array( $this, 'is_plugin_unblocked' ) );
 	}
 
 	function pre_update_option_active_plugins( $newvalue, $oldvalue ) {
 		// removed all blocked plugins
-		$diff = array_diff( $newvalue, $oldvalue );
+		$diff     = array_diff( $newvalue, $oldvalue );
 		$newvalue = array_filter( $diff, array( $this, 'is_plugin_unblocked' ) );
 		return $newvalue;
 	}
@@ -142,14 +162,9 @@ class Plugin_Control {
 	function is_plugin_blocked( $plugin ) {
 		return is_int( array_search( $plugin, $this->blocklist ) );
 	}
+
 	function is_plugin_unblocked( $plugin ) {
 		return !$this->is_plugin_blocked( $plugin );
 	}
 
 }
-if ( ! function_exists( 'printer') ) {
-	function printer( $input ) {
-		echo '<pre>' . print_r( $input, true ) . '</pre>';
-	}
-}
-// eof
